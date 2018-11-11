c9517a4292998d891ac8f8fd4d48790086b93bb0
vini23hot <vini23hot@gmail.com>
2018-11-11 10:59:02 -0200

    Added Airbnb style guide

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 import './index.css';
 
-ReactDOM.render(<h1>crypto-status</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));

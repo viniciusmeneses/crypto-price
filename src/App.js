cc5f844e365b7d7e9dbba66629a4e33aacf0830d
vini23hot <vini23hot@gmail.com>
2018-11-13 17:14:53 -0200

    Renamed the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Menu from './components/Nav/Menu';
-import Status from './components/Main/Status';
+import Container from './components/Main/Container';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -29,11 +29,11 @@ export default class App extends Component {
       <div className="app container d-flex flex-column justify-content-center align-items-center p-3">
         <header>
           <h1 className="mb-4 text-center">
-            crypto-status
+            crypto-price
           </h1>
         </header>
         <Menu coinSelected={this.handleCoinSelect} />
-        <Status coin={coin} />
+        <Container coin={coin} />
       </div>
     );
   }
--- src/components/Main/Container.jsx
+++ src/components/Main/Container.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import PriceChart from './Charts/Price';
 
-import './Status.css';
+import './Container.css';
 
 export default (props) => {
   const { coin } = props;
   return (
-    <main className="status container-fluid pt-3 pl-2 pr-3 pb-2">
+    <main className="main-container container-fluid pt-3 pl-2 pr-3 pb-2">
       <PriceChart coin={coin} />
     </main>
   );
